94ae1773eec10321912c35c615d5f2a52417565d
user <user@Admins-iMac.local>
2025-10-27 18:18:14 +0300

    Updates

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  base: '/kihungi-folio/', // make sure this matches your GitHub repo name exactly
-  plugins: [react()],
-})
